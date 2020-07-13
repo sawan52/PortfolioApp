8da4a127071d54f6b1810cbc772cd86e10e82c93
sawan5216977 <sawansingh5216977@gmail.com>
2020-07-13 12:43:29 +0530

    side menu added

--- app/src/main/java/com/example/portfolioapp/MainActivity.java
+++ app/src/main/java/com/example/portfolioapp/MainActivity.java
@@ -3,15 +3,27 @@ package com.example.portfolioapp;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.portfolioapp.cv.CVFragment;
 import com.example.portfolioapp.home.HomeFragment;
 import com.example.portfolioapp.portfolio.PortfolioFragment;
+import com.example.portfolioapp.sidemenu.Callback;
+import com.example.portfolioapp.sidemenu.MenuAdapter;
+import com.example.portfolioapp.sidemenu.MenuItem;
+import com.example.portfolioapp.sidemenu.MenuUtil;
 import com.example.portfolioapp.team.TeamFragment;
 
+import java.util.List;
 import java.util.Objects;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Callback {
+
+    RecyclerView RvMenu;
+    List<MenuItem> menuItems;
+    MenuAdapter adapter;
+    int selectedMenuPos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +32,32 @@ public class MainActivity extends AppCompatActivity {
 
         Objects.requireNonNull(getSupportActionBar()).hide();
 
+        // setup side menu
+
+        setupSideMenu();
+
+        // set the default fragment when activity launches
+
+        setHomeFragment();
+
         //setHomeFragment();
         //setCVFragment();
         //setTeamFragment();
         setPortfolioFragment();
+
+    }
+
+    private void setupSideMenu() {
+
+        RvMenu = findViewById(R.id.rv_side_menu);
+
+        // get menu list items which will get data from a static data
+
+        menuItems = MenuUtil.getMenuList();
+
+        adapter = new MenuAdapter(menuItems, this);
+        RvMenu.setLayoutManager(new LinearLayoutManager(this));
+        RvMenu.setAdapter(adapter);
 
     }
 
@@ -43,4 +77,34 @@ public class MainActivity extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction().replace(R.id.container, new PortfolioFragment()).commit();
     }
 
+    @Override
+    public void onSideMenuItemClick(int i) {
+
+        switch (menuItems.get(i).getCode()) {
+
+            case MenuUtil.HOME_FRAGMENT_CODE:
+                setHomeFragment();
+                break;
+            case MenuUtil.CV_FRAGMENT_CODE:
+                setCVFragment();
+                break;
+            case MenuUtil.TEAM_FRAGMENT_CODE:
+                setTeamFragment();
+                break;
+            case MenuUtil.PORTFOLIO_FRAGMENT_CODE:
+                setPortfolioFragment();
+                break;
+            default:
+                setHomeFragment();
+
+        }
+
+        // highlight the selected menu items
+
+        menuItems.get(selectedMenuPos).setSelected(false);
+        menuItems.get(i).setSelected(true);
+        selectedMenuPos = i;
+        adapter.notifyDataSetChanged();
+
+    }
 }
---
+++ app/src/main/java/com/example/portfolioapp/sidemenu/Callback.java
@@ -0,0 +1,7 @@
+package com.example.portfolioapp.sidemenu;
+
+public interface Callback {
+
+    void onSideMenuItemClick(int i);
+
+}
